cadde96f16763783a8bac1676906316508a8d738
armandwipangestu <armandwi.pangestu7@gmail.com>
2023-10-19 16:34:02 +0700

    feat: add tuple data type

--- tests/array.test.ts
+++ tests/array.test.ts
@@ -9,6 +9,15 @@ describe("Array", function () {
 
   it("should support readonly array", function () {
     const hobbies: ReadonlyArray<string> = ["Membaca", "Menulis"];
+
     console.info(hobbies);
+  });
+
+  it("should support tupple", function () {
+    const person: readonly [string, string, number] = ["Arman", "Dwi", 21];
+
+    console.info(person[0]);
+    console.info(person[1]);
+    console.info(person[2]);
   });
 });
