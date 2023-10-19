fa9c8fd8ca0649023549178b39db07cbe56503b8
armandwipangestu <armandwi.pangestu7@gmail.com>
2023-10-19 16:06:05 +0700

    feat: add array data type

---
+++ dist/tests/array.test.js
@@ -0,0 +1,9 @@
+"use strict";
+describe("Array", function () {
+    it("should same with javascript", function () {
+        const names = ["Arman", "Dwi", "Pangestu"];
+        const values = [1, 2, 3];
+        console.info(names);
+        console.info(values);
+    });
+});
---
+++ tests/array.test.ts
@@ -0,0 +1,9 @@
+describe("Array", function () {
+  it("should same with javascript", function () {
+    const names: string[] = ["Arman", "Dwi", "Pangestu"];
+    const values: number[] = [1, 2, 3];
+
+    console.info(names);
+    console.info(values);
+  });
+});
