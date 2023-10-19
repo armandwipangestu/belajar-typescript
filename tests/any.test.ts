0f731a80d9d12863f6a79a7756ff1966d7b23cbf
Arman <64394320+armandwipangestu@users.noreply.github.com>
2023-10-19 16:45:54 +0700

    feat: add any data type (#5)

---
+++ tests/any.test.ts
@@ -0,0 +1,14 @@
+describe("Any", function () {
+  it("should support in typescript", function () {
+    const person: any = {
+      id: 1,
+      name: "Arman Dwi Pangestu",
+      age: 21,
+    };
+
+    person.age = 22;
+    person.address = "Indonesia";
+
+    console.info(person);
+  });
+});
